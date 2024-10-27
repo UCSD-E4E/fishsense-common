a5baf9b58af77c3daca0bf9b96c7c937a634a429
Christopher L. Crutchfield <ccrutchf@ucsd.edu>
2024-10-26 20:15:21 -0700

    feat: allow overriding num_cpu and num_gpu

--- fishsense_common/pluggable_cli/command.py
+++ fishsense_common/pluggable_cli/command.py
@@ -4,14 +4,16 @@ Base class for cli commands which allows for adding additional arguments to the 
 
 from abc import abstractmethod
 from logging import Logger
+from multiprocessing import cpu_count
 from pathlib import Path
 
 import ray
+import torch
 import yaml
 from appdirs import user_config_dir
 
 from fishsense_common import __version__
-from fishsense_common.pluggable_cli.arguments import ARGUMENTS
+from fishsense_common.pluggable_cli.arguments import ARGUMENTS, argument
 
 
 class Command:
@@ -41,8 +43,28 @@ class Command:
     def allow_config(self) -> bool:
         return True
 
+    @property
+    @argument("--max-cpu", help="Sets the maximum number of CPU cores allowed.")
+    def max_num_cpu(self) -> int:
+        return self.__max_num_cpu
+
+    @max_num_cpu.setter
+    def max_num_cpu(self, value: int):
+        self.__max_num_cpu = value
+
+    @property
+    @argument("--max-gpu", help="Sets the maximum number of GPU kernels allowed.")
+    def max_num_gpu(self) -> int:
+        return self.__max_num_gpu
+
+    @max_num_gpu.setter
+    def max_num_gpu(self, value: int):
+        self.__max_num_gpu = value
+
     def __init__(self) -> None:
         self.__logger: Logger = None
+        self.__max_num_cpu: int = None
+        self.__max_num_gpu: int = None
 
     def save_config(self, save_config: str):
         class_name = f"{self.__class__.__module__}.{self.__class__.__qualname__}"
@@ -66,6 +88,16 @@ class Command:
         if ray_config_path.exists():
             with ray_config_path.open("r") as f:
                 ray_config = yaml.safe_load(f)
+
+        # Allow override of num_cpus and num_gpus.
+        if self.max_num_cpu is not None:
+            ray_config["num_cpus"] = min(cpu_count(), self.__max_num_cpu or 0)
+
+        if self.max_num_gpu is not None:
+            ray_config["num_gpus"] = min(
+                torch.cuda.device_count() if torch.cuda.is_available() else 1000,
+                self.__max_num_gpu or 0,
+            )
 
         ray.init(**ray_config)
 
