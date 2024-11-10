41fba984ca29e8cd673b3844da59c2fb2ab87abd
Christopher L. Crutchfield <ccrutchf@ucsd.edu>
2024-11-09 23:23:49 -0800

    feat: allow specifying vram instead of total portion of the gpu

---
+++ fishsense_common/ray/decorators.py
@@ -0,0 +1,14 @@
+import math
+
+import ray
+import torch
+
+
+def execute(vram_mb: int):
+    available_vram_mb = torch.cuda.get_device_properties(0).total_memory / 1024**2
+    percent_of_available_vram = vram_mb / available_vram_mb
+
+    if percent_of_available_vram > 1:
+        percent_of_available_vram = math.ceil(percent_of_available_vram)
+
+    return ray.execute(num_gpus=percent_of_available_vram)
